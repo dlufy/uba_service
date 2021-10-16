d4f15002112948033d1a58f5413fdfbcbb623ebf
dlufy <ajayynitkkr@gmail.com>
2021-10-17 00:53:05 +0530

    removed config

--- database/db.js
+++ database/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
-const config = require("../config/config.json")
+// const config = require("../config/config.json")
 
 const connectDb = ()=> {
-    return mongoose.connect(`mongodb+srv://gatekeeper:${process.env.MongoPassword|config.Database.Password}@cluster0.yjxfz.mongodb.net/felinos?retryWrites=true&w=majority`);
+    return mongoose.connect(`mongodb+srv://gatekeeper:${process.env.MongoPassword}@cluster0.yjxfz.mongodb.net/felinos?retryWrites=true&w=majority`);
 }
  
 module.exports = connectDb;
